60c6a68beed0f113161db4fe62c0f025f4d28d2f
AI1411 <goethe0719@gmail.com>
2020-01-01 14:55:46 +0900

    モデル作成とダミーデータ挿入

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,19 @@
+import router from "./routes";
+import VueRouter from "vue-router";
+import Index from "./Index";
+
 require('./bootstrap');
+
+window.Vue = require('vue');
+
+Vue.use(VueRouter);
+
+Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+
+const app = new Vue({
+    el: '#app',
+    router, VueRouter,
+    components: {
+        Index
+    }
+});
---
+++ resources/js/bookables/Bookables.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="container">
+        <h1>Title</h1>
+        <bookable-list-item
+            :item-title="bookable1.title"
+            :item-content="bookable1.content"
+            :price="1500">
+        </bookable-list-item>
+    </div>
+</template>
+
+<script>
+    import BookableListItem from "./BookableListItem";
+
+    export default {
+        components: {
+            BookableListItem
+        },
+        data() {
+            return {
+                bookable1: {
+                    title: null,
+                    content: null
+                },
+                bookable2: {
+                    title: 'kdvna:',
+                    content: 'dkanv;kd'
+                }
+            }
+        },
+        created() {
+            console.log(this.bookable1);
+            console.log(this.bookable2);
+
+            setTimeout(() => {
+                this.bookable1 = {
+                    title :'Expensive villa',
+                    content: 'a very cheap villa'
+                }
+            }, 5000)
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
